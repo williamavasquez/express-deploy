739980d4b8076902ccf4edf6d96c87c7a3d90eac
William Vasquez <william.a.vasquez@gmail.com>
2019-02-09 12:07:30 -0500

    initial files

---
+++ client/build/precache-manifest.de2403d8346d6c73d801b61140690a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96234ec7e7264ccaa5e8",
+    "url": "/static/js/main.96234ec7.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "96234ec7e7264ccaa5e8",
+    "url": "/static/css/main.939684af.chunk.css"
+  },
+  {
+    "revision": "d78c2ba4544d391db864f0fd81361806",
+    "url": "/index.html"
+  }
+];
